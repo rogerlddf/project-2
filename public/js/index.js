79fab10467b4564608ac3733ecfecfdbe84aea40
Andrey Nelepov <nelepow@gmail.com>
2019-06-27 15:26:56 -0400

    added options playlists

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,10 @@ $(function () {
   // let movieList = $("#movie-list");
   // let movieList = document.getElementById('movie-list');
   let movieListAll = document.getElementById('allmovie-list');
+  let playlistSelect = $("#playlist");
+  let playlistId;
+  // Getting the playlists, and their posts
+  getPlaylists();
 
 
   // ALL MOVIES ON SUBMIT
@@ -16,6 +20,57 @@ $(function () {
     $("#allmovie-list").css('overflow', 'auto');
     $('#all-movies-row').css('display', 'block');
     $('.jumbotron').hide(400);
+
+     var url = window.location.search;
+      var movieId;
+      var playlistId;
+
+    var updating = false;
+
+    // If we have this section in our url, we pull out the post id from the url
+    // In '?post_id=1', postId is 1
+    if (url.indexOf("?movie_id=") !== -1) {
+      movieId = url.split("=")[1];
+      getMovieData(movieId, "movie");
+    }
+    // Otherwise if we have an author_id in our url, preset the author select box to be our Playlist
+    else if (url.indexOf("?playlist_id=") !== -1) {
+      playlistId = url.split("=")[1];
+    }
+
+    if (updating) {
+      newMovie.id = movieId;
+      updateMovie(newMovie);
+    }
+
+    // Gets post data for the current post if we're editing, or if we're adding to an palylist's existing movies
+    function getMovieData(id, type) {
+      var queryUrl;
+      switch (type) {
+        case "movie":
+          queryUrl = "/api/movies/" + id;
+          break;
+        case "playlist":
+          queryUrl = "/api/playlists/" + id;
+          break;
+        default:
+          return;
+      }
+      $.get(queryUrl, function (data) {
+        if (data) {
+          console.log(data.PlaylistId || data.id);
+          // If this movie exists, prefill our cms forms with its data
+          titleInput.val(data.title);
+          bodyInput.val(data.year);
+          authorId = data.PlaylistId || data.id;
+          // If we have a post with this id, set a flag for us to know to update the post
+          // when we hit submit
+          updating = true;
+        }
+      });
+    }
+
+
     var getUrl = "https://www.omdbapi.com/?s=" + searchText + "&apikey=13a937dc&type=movie"
     $.ajax({
       url: getUrl,
@@ -61,7 +116,37 @@ $(function () {
     $.post('/api/movies', chosenMovie, function (data) {
       console.log(data); // John
     }, "json");
-  })
+  });
+
+  // A function to get Playlists and then render our list of Authors
+  function getPlaylists() {
+    $.get("/api/playlists", renderPlayList);
+  }
+  // Function to either render a list of authors, or if there are none, direct the user to the page
+  // to create an author first
+  function renderPlayList(data) {
+    
+    // $(".hidden").removeClass("hidden");
+    var rowsToAdd = [];
+    for (var i = 0; i < data.length; i++) {
+      rowsToAdd.push(createPlaylistRow(data[i]));
+    }
+    playlistSelect.empty();
+    console.log(rowsToAdd);
+    console.log(playlistSelect);
+    playlistSelect.append(rowsToAdd);
+    playlistSelect.val(playlistId);
+  }
+
+  // Creates the playlist options in the dropdown
+  function createPlaylistRow(playlist) {
+    var listOption = $("<option>");
+    listOption.attr("value", playlist.id);
+    listOption.text(playlist.name);
+    return listOption;
+  }
+
+
  
 
 });
